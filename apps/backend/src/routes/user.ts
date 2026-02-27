13d85b13341efe35fdae8833f11ce4512c935ab8
Daniel Mercado <60846433+Damet24@users.noreply.github.com>
2026-02-26 19:50:31 -0500

    Update user.ts

--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/UserController'
+import { authenticate } from '../middlewares/authenticate'
+import { authorize } from '../middlewares/authorize'
+import { UserRole } from '@packages/domain/index'
 
 const userRouter = Router()
 
 const userController = new UserController()
 
 userRouter.get('/', userController.index.bind(userController))
+userRouter.get('/me', authenticate, authorize(UserRole.ADMIN), userController.me.bind(userController))
 
 export default userRouter as Router
