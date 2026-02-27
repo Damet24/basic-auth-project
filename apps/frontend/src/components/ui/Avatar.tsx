cbf85dc7fff41e720e6c3955d427e91bf75dea94
Daniel Mercado <60846433+Damet24@users.noreply.github.com>
2026-02-26 23:13:23 -0500

    ui components added

---
+++ apps/frontend/src/components/ui/Avatar.tsx
@@ -0,0 +1,8 @@
+export function Avatar() {
+  return (
+    <div className="w-10 h-10 rounded-full bg-blue-600 
+    text-white flex items-center justify-center font-bold">
+      JP
+    </div>
+  );
+}
