86e967d16708a1395ff796ac4dc0c8f7b307a9d1
Daniel Mercado <60846433+Damet24@users.noreply.github.com>
2026-02-26 19:49:42 -0500

    new route for user info

--- apps/backend/src/controllers/UserController.ts
+++ apps/backend/src/controllers/UserController.ts
@@ -1,9 +1,18 @@
 import type { Request, Response } from 'express'
 import { userService } from '../di'
+import { AuthenticatedRequest } from '../types/AuthenticatedRequest'
 
 export class UserController {
   async index(_req: Request, res: Response) {
     const result = await userService.getAll()
     return res.json(result)
+  }
+
+  async me(req: AuthenticatedRequest, res: Response) {
+    if (!req.user) {
+      return res.status(400).json({ error: 'User not authenticated' })
+    }
+    const user = await userService.getById(req.user.userId)
+    return res.json(user)
   }
 }
