f5bffc24608c23c2f58463ec0111dc04034ee11c
Daniel Mercado <60846433+Damet24@users.noreply.github.com>
2026-02-26 21:14:41 -0500

    not found route handler

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -20,6 +20,11 @@ app.use(helmet.frameguard({ action: 'deny' }))
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
 
+app.use((req: Request, res: Response, _next: NextFunction) => {
+  logger.info(`${req.method} - ${req.url} - ${httpStatus[404]}`)
+  res.status(httpStatus.NOT_FOUND).json({ error: httpStatus.NOT_FOUND, body: httpStatus[404] });
+})
+
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   logger.error(err.stack)
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Internal Server Error");
