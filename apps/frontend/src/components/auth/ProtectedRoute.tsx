8cfe7760052f4ae5c63bff7f45fc8300c20cc6ed
Daniel Mercado <60846433+Damet24@users.noreply.github.com>
2026-02-26 23:13:02 -0500

    Create ProtectedRoute.tsx

---
+++ apps/frontend/src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router";
+import { useAuth } from "../../contexts/AuthContext";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function ProtectedRoute({ children }: Props) {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <>{children}</>;
+}
